3b0ea9db13fab177427b1f5b840b52ec300cf748
tigerish009 <64354659+tigerish009@users.noreply.github.com>
2020-09-05 16:07:26 +0900

    web2-nodejs 200905

---
+++ syntax/variable2.js
@@ -0,0 +1,5 @@
+var name = 'egoing';
+
+var letter = 'Dear ' + name + ' Incididunt labore laborum laboris ex pariatur ullamco aliqua aliqua in sit. Aliqua labore reprehenderit aute fugiat. Consequat eu do labore consectetur fugiat elit incididunt esse velit id eiusmod. Reprehenderit ut aliqua enim adipisicing duis pariatur eu incididunt laboris voluptate ex cupidatat. Non irure exercitation nostrud cupidatat tempor in aute. Aliqua enim nostrud velit ut quis deserunt est aliquip pariatur aliqua nulla irure. Nisi incididunt occaecat ea ut sint dolor incididunt qui deserunt voluptate sunt ullamco.'
+
+console.log(letter); 
