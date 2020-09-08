1eea9ae83ed25fb442ed5c2cd6a048ff7e512c71
tigerish009 <64354659+tigerish009@users.noreply.github.com>
2020-09-08 21:27:18 +0900

    writeFIle & redirection 200908

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
  
 function templateHTML(title, list, body){
   return `
@@ -60,7 +61,7 @@ var app = http.createServer(function(request,response){
         var title = 'WEB - create';
         var list = templateList(filelist);
         var template = templateHTML(title, list, `
-          <form action="http://localhost:3000/process_create" method="post">
+          <form action="http://localhost:3000/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -73,12 +74,23 @@ var app = http.createServer(function(request,response){
         response.writeHead(200);
         response.end(template);
       });
+    } else if(pathname === '/create_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description;
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){ //form submit 하면 파일 생성됨
+            response.writeHead(302, {Location: `/?id=${title}`}); //302 리다이레트, submit하면 해당페이지로 바로 이동해줌
+            response.end();
+          })
+      });
     } else {
       response.writeHead(404);
       response.end('Not found');
     }
- 
- 
- 
 });
 app.listen(3000);
